40877c13244e0736f7245144114f41ba055be654
Erik <erik@familie-dierkes.de>
2022-01-24 17:54:48 +0100

    Removed unused imports

--- Projekt/tictactoe/lib/main.dart
+++ Projekt/tictactoe/lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:tictactoe/sites/settings_page.dart';
 import 'package:tictactoe/sites/spiele_page.dart';
 
